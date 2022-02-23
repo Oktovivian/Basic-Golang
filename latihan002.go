999ea3599948b5caec27d85288aed368fe3bb7f0
Oktovivian <68942070+Oktovivian@users.noreply.github.com>
2022-02-23 10:49:26 +0700

    Add files via upload

---
+++ latihan002.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	// deklarasi variabel
+
+	// bisa pake gini
+	var i, j, k int
+	i = 5
+	j = 6
+	k = 10
+	fmt.Println(i, j, k)
+
+	// bisa pake gini
+	var f float64
+	f = 3.7
+	fmt.Println(f)
+
+	// bisa pake gini
+	var b bool
+	var s string
+	b = true
+	s = "saya sedang belajar"
+	fmt.Print(b, s)
+
+	// bisa pake gini
+	variable_string := "100"
+	print(variable_string)
+
+	// bisa pake gini
+	var x, y, a int
+	var z float64
+	x = 10
+	y = 2
+	z = float64(x) / float64(y)
+	a = x / y
+	fmt.Print(z, a)
+}
---
+++ latihan003.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var x, y float64
+	x = 5
+	y = 2
+	fmt.Print(math.Pow(x, y))
+}
